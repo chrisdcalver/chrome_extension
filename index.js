28580666bb2253c64d5f50fd3f02fed7b712a609
chrisdcalver <chrisdcalver@gmail.com>
2021-07-10 20:04:50 +0100

    Event Listener functions

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epilead.com", "www.greatlead.com"]
 let inputEl = document.getElementById("input-el").nodeValue
 
 let inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
 })
+
+// Log out the items in the myLeads array using a for loop
+
+for (i = 0; i < myLeads.length; i++) {
+    ulEl.textContent += myLeads[i]
+}
